c57619a4b2045d7ec4ecbfc76fa25aa0d36753d3
Adam-Harte <adamharte11@hotmail.co.uk>
2020-03-29 13:35:03 +0100

    methods in classes vs constructor functions added

--- app.js
+++ app.js
@@ -19,3 +19,40 @@ newPerson.greet();
 // prototypes
 console.log(newPerson.__proto__);
 newPerson.logAge();
+
+// methods in classes vs constructor functions
+class Book {
+  constructor() {
+    this.title = 'Way of Kings';
+    this.author = 'Sanderson';
+    this.releaseDate = 2014;
+  }
+
+  logBook() {
+    console.log(this.title);
+    console.log(this.author);
+    console.log(this.releaseDate);
+  }
+}
+
+const b1 = new Book();
+const b2 = new Book();
+
+console.log(b1.__proto__ === b2.__proto__);
+
+function Movie() {
+  this.title = 'Spiderman';
+  this.releaseDate = 2007;
+
+  this.logTitle = function() {
+    console.log(thus.title);
+  };
+}
+
+Movie.prototype.logRelease = function() {
+  console.log(this.releaseDate);
+};
+
+const newMovie = new Movie();
+
+console.log(newMovie.__proto__);
